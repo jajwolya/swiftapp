71672245462dff92f734f66c888a2773f94d43ae
SamuraiYak <jajwolya@outlook.com>
2017-02-15 14:10:58 +1100

    My first commit

--- Tutorial/ViewController.swift
+++ Tutorial/ViewController.swift
@@ -10,10 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var headLabel: UILabel!
+    
+    var counter = 0
+
+    @IBAction func buttonTapped(_ sender: Any) {
+        headLabel.text = "This is not a string"
+        counter += 1
+        print(counter)
+        if counter >= 10 {
+            headLabel.text = "You have pressed me too many times, please stop"
+        }
+    }
+    
+    @IBAction func secondButton(_ sender: Any) {
+        headLabel.text = "Stop it"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
